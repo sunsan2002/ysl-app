efe7080abc5f64835a04d10d724f0ac0701e45ee
sunsan <2390864551@qq.com>
2024-04-08 20:10:29 +0800

    报名页面

---
+++ pages/index/apply.vue
@@ -0,0 +1,260 @@
+<template>
+	<view class="container">
+		<image src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcT3t_pT8Xm39dPS1FE89-yt9ab3SN1xpNRP9Jl-LlaETE5itBsG"></image>
+		<view class="top">
+			<p>AI引领 智启新程</p>
+			<view class="msg" v-for="item in state.detail" :key="item.icon">
+				<uni-icons color="#8b8b8b" :type="item.icon" size="20"></uni-icons>
+				<span>{{ item.text }}</span>
+			</view>
+			<view class="share">
+				<uni-icons color="#848484" type="redo-filled" size="20"></uni-icons>
+				分享
+			</view>
+			<view class="process">未开始</view>
+		</view>
+		<view class="content">
+			<view class="tab-header">
+				<view class="tab-item" :class="{ active: currentTab === 1 }" @click="switchTab(1)">会议介绍</view>
+				<view class="tab-item" :class="{ active: currentTab === 2 }" @click="switchTab(2)">会议流程</view>
+			</view>
+			<view class="tab-content">
+				<view class="page1" v-if="currentTab === 1">
+					<span>
+						行业内首次大规模的AI新品全景展示，将全面展示安恒信息在人工智能领域的最新研发成果，更将AI技术与实际应用场景紧密结合，呈现安恒信息在技术创新和产品研发方面的实力。
+					</span>
+					<image src="https://prod-tx-mdn-1305533294.file.myqcloud.com/static/1775046527844352.jpg"></image>
+				</view>
+				<view v-if="currentTab === 2">
+					<view class="page1">
+						<view v-for="item in state.process" :key="item.id" class="ax-item">
+							<view class="ax-mark">
+								<view class="ax-node"></view>
+							</view>
+							<view class="ax-text">
+								<view class="ax-title">
+									<span>{{item.time}}</span>
+									<h3>{{item.title}}</h3>
+								</view>
+								<view class="ax-des">&nbsp;&nbsp;&nbsp;</view>
+							</view>
+						</view>
+					</view>
+				</view>
+			</view>
+		</view>
+		<view class="apply-btn">我要报名</view>
+	</view>
+</template>
+
+<script setup>
+import { reactive, ref } from 'vue';
+
+const currentTab = ref(2);
+
+function switchTab(item) {
+	currentTab.value = item;
+}
+
+const state = reactive({
+	detail: [
+		{
+			icon: 'calendar',
+			text: '2023年2月23日-2023年2月28日'
+		},
+		{
+			icon: 'map-pin-ellipse',
+			text: '杭州洲际酒店·国际厅2厅'
+		},
+		{
+			icon: 'person',
+			text: '刘博'
+		},
+		{
+			icon: 'staff',
+			text: '参会人数200人'
+		}
+	],
+	process:[
+		{
+			id:1,
+			time:'09:00-09:30',
+			title:'媒体签到，暖场视频'
+		},
+		{
+			id:2,
+			time:'09:30 - 09:35',
+			title:'主持人开场'
+		},
+		{
+			id:5,
+			time:'09:40-10:00',
+			title:'领导致辞'
+		},
+		{
+			id:6,
+			time:'09:45 - 09:50',
+			title:'发布仪式：DAS三大核心能力图谱OR智能体全景图发布'
+		},
+		{
+			id:10,
+			time:'12:25 - 12:25',
+			title:'发布会结束'
+		},
+	]
+});
+</script>
+
+<style lang="scss" scoped>
+.container {
+	min-height: 100vh;
+	background-color: #fafafa;
+}
+.container > image {
+	width: 100vw;
+	height: 11rem;
+	object-fit: contain;
+}
+.top {
+	padding: 20rpx;
+	background-color: #fff;
+	position: relative;
+	margin-bottom: 20rpx;
+	> p {
+		font-weight: bold;
+	}
+	.share {
+		position: absolute;
+		right: 0;
+		background-color: #e3e3e3;
+		display: flex;
+		justify-content: row;
+		align-items: center;
+		font-size: 22rpx;
+		border-bottom-left-radius: 20rpx;
+		border-top-left-radius: 20rpx;
+		padding: 5rpx 10rpx;
+		color: #848484;
+		top: 30rpx;
+	}
+	.process {
+		position: absolute;
+		right: 60rpx;
+		top: 120rpx;
+		background-color: rgba(0, 198, 210, 0.2);
+		color: #00c6d2;
+		padding: 10rpx 20rpx;
+		border-radius: 15rpx;
+		font-size: 25rpx;
+	}
+}
+.msg {
+	color: #8b8b8b;
+	font-size: 25rpx;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	gap: 10rpx;
+	margin-top: 6rpx;
+}
+.tab-header {
+	display: flex;
+	background-color: #fff;
+	justify-content: space-around;
+}
+
+.tab-item {
+	text-align: center;
+	padding: 25rpx;
+	border: none;
+	cursor: pointer;
+	font-weight: bold;
+	letter-spacing: 2rpx;
+	font-size: 30rpx;
+}
+
+.active {
+	border-bottom: 6rpx solid #00c6d2;
+	color: #00c6d2;
+}
+
+.tab-content {
+	margin-top: 10rpx;
+}
+
+.ax-item {
+	width: 100%;
+	flex: none;
+	display: flex;
+	margin-left: 2rem;
+	
+}
+.ax-mark {
+	height: 100%;rpxidth: 14rpx;
+	display: flex;
+	flex-direction: column;
+	box-sizing: border-box;
+}
+.line {
+	height: calc(100% - 1.4rem);
+	top: 1.4rem;
+	position: absolute;
+}
+.ax-text {
+	text-align: left;
+	margin-left: 1.4rem;
+	flex: auto;
+	margin-top: 0;
+	width: 0;
+}
+.ax-title {
+	padding: 6rpx;
+	width: 90%;
+}
+.ax-node {
+	width: 23rpx;
+	height: 23rpx;
+	border-radius: 50%;
+	background-color: #00c6d2;
+	display: inline-block;
+	position: relative;
+	&:after {
+	content: '';
+	height: 240rpx;
+	width: 5rpx;
+	background-color: #00c6d2;
+	position: absolute;
+	top: 0;
+	left: 50%;
+	right: 50%;
+	transform: translateX(-50%);
+}
+}
+
+
+.page1 {
+	padding: 30rpx;
+	color: #6a6a6a;
+	line-height: 50rpx;
+	font-size: 29rpx;
+	background-color: #fff;
+	> image {
+		min-width: 100%;
+		margin-top: 15rpx;
+	}
+}
+.apply-btn {
+	position: fixed;
+	bottom: 30rpx;
+	background-color: #00c6d2;
+	right: 30rpx;
+	left: 30rpx;
+	padding: 25rpx;
+	display: flex;
+	justify-content: center;
+	border-radius: 30rpx;
+	color: #fff;
+	font-weight: bold;
+	letter-spacing: 10rpx;
+}
+</style>
